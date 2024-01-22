e2edf8bc965244b1211bb8255884b92c46c22d8d
jojunhyeong <jojh0323@naver.com>
2024-01-22 17:10:06 +0900

    뷰바인딩을 이용한 UI요소 가져오기

--- app/src/main/java/com/example/unittransducer/MainActivity.kt
+++ app/src/main/java/com/example/unittransducer/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.unittransducer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.unittransducer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val outputTextView = binding.outputTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val inputEditText = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
+
     }
 }

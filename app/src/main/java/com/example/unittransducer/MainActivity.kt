d511fd66fcf60a8e98d6a84e0a76a11ccaec7c26
jojunhyeong <jojh0323@naver.com>
2024-01-22 17:35:25 +0900

    단위변환 기능 구현

--- app/src/main/java/com/example/unittransducer/MainActivity.kt
+++ app/src/main/java/com/example/unittransducer/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.unittransducer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import com.example.unittransducer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +19,37 @@ class MainActivity : AppCompatActivity() {
         val outputUnitTextView = binding.outputUnitTextView
         val inputEditText = binding.inputEditText
         val inputUnitTextView = binding.inputUnitTextView
+        val swapImageButton = binding.swapImageButton
+
+        var inputNumber : Int = 0
+        var cmToM = true
+
+        inputEditText.addTextChangedListener { text ->
+            inputNumber = if(text.isNullOrEmpty()) {
+                0
+            } else {
+                text.toString().toInt()
+            }
+
+            if(cmToM) {
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
+        swapImageButton.setOnClickListener{
+            cmToM = cmToM.not()
+            if(cmToM) {
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
 
     }
 }
